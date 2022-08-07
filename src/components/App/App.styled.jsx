bc63858534dc7b2d2344777bd6966e3b5c0b3ae6
IrynaShashko <irishkashashko@gmail.com>
2022-08-07 12:24:51 +0300

    add style

--- src/components/App/App.styled.jsx
+++ src/components/App/App.styled.jsx
@@ -14,7 +14,7 @@ export const PhonebookContainer = styled.div`
   padding: 30px;
   border: 1px solid #ccc;
   border-radius: 50px;
-  background-color: #FBFAF0;
+  background-image: linear-gradient(to bottom, #d37fae, #c585bb, #b68bc4, #a591ca, #9596cd, #8e92c5, #878dbd, #8189b5, #7e7ca4, #7a6f93, #746382, #6c5771);
   box-shadow: 0px 4px 5px 4px rgba(0, 0, 0, 0.75);
 `;
 
--- src/components/Contacts/Contacts.styled.jsx
+++ src/components/Contacts/Contacts.styled.jsx
@@ -17,7 +17,7 @@ export const ContactsItem = styled.li`
   margin-bottom: 10px;
   border: 1px solid #fff;
   border-radius: 50px;
-  background-color: #f5f382;
+  background-color: #d37fae;
   box-shadow: 0px 0px 2px 2px rgba(0, 0, 0, 0.75);
   &:hover,
   &:focus {
